7f9e7063ecb88eff0818d9a58880754e0de84c85
KumariSonia <k.sonia15896@gmail.com>
2023-01-22 13:17:41 +0530

    Added changes in User for one to many mapping

---
+++ src/main/java/com/sonia/DemoHibernate/model/User.java
@@ -0,0 +1,57 @@
+package com.sonia.DemoHibernate.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+	@Id
+	private int userId;
+	private String userName;
+	private String userAddress;
+	@OneToMany
+	private List<Book> book = new ArrayList<Book>();
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	public List<Book> getBook() {
+		return book;
+	}
+
+	public void setBook(List<Book> book) {
+		this.book = book;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userAddress=" + userAddress + ", book=" + book
+				+ "]";
+	}
+
+}
