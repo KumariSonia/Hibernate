1fab5b9efe6ed925df2c9e5e0fcf96ad96ad0df7
KumariSonia <k.sonia15896@gmail.com>
2023-01-22 23:10:42 +0530

    Added changes in App.js for first level cache fetchinh

--- src/main/java/com/sonia/DemoHibernate/App.java
+++ src/main/java/com/sonia/DemoHibernate/App.java
@@ -1,14 +1,11 @@
 package com.sonia.DemoHibernate;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import com.sonia.DemoHibernate.model.Book;
-import com.sonia.DemoHibernate.model.User;
+import com.sonia.DemoHibernate.model.Customer;
 
 /**
  * Hibernate Examples
@@ -20,28 +17,23 @@ public class App {
 		/*
 		 * Save Data
 		 */
-
-	User user = null;
-
-		Configuration configuration = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Book.class)
-				.addAnnotatedClass(User.class);
+		Customer customer = null;
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Customer.class);
 
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
 
 		Session session = sessionFactory.openSession();
 
 		Transaction transaction = session.beginTransaction();
-		
-		user = session.get(User.class, 101);
-		
+
+		customer = session.get(Customer.class, 56);
+		System.out.println(customer);
+
+		customer = session.get(Customer.class, 56);  //will not fire query again
+		System.out.println(customer);
+
 		transaction.commit();
-		
-//		List<Book> books = user.getBook();
-//		
-//		for(Book book : books)
-//		{
-//			System.out.println(book.getBookId());
-//		}
 
 	}
 }
